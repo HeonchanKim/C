1a1e50c7daeab7414c073ce008bafd5243023486
HeonchanKim <khc9274@naver.com>
2022-06-14 23:20:47 +0900

    동적 할당 및 구조체

---
+++ workspace/day34/structure/structTest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Animal {
+	char name[200];
+	int age;
+	char feed[200];
+};
+
+void main() {
+	/*struct Animal dog = { "Max", 10, "°³²­" };*/
+	struct Animal dog;
+	strcpy_s(dog.name, sizeof(dog.name), "max");
+	dog.age = 20;
+	strcpy_s(dog.feed, sizeof(dog.feed), "°³²­");
+
+	printf("%s, %d, %s", dog.name, dog.age, dog.feed);
+}
